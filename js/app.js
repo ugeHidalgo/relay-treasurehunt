91397654acd21d4317767506cf02247037d9aa6d
ugeHidalgo <ugeHidalgo@hotmail.com>
2016-03-14 20:07:16 +0100

    Added react-router-relay to project.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
-import 'babel-polyfill';
-
-import App from './components/App';
-import AppHomeRoute from './routes/AppHomeRoute';
+//instal react-router react-relay y react-router-relay
+//npm install --save react react-dom react-relay react-router
+//npm install --save react-router-relay
+import {createHashHistory} from 'history';
+import {IndexRoute, Route} from 'react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Relay from 'react-relay';
+import {RelayRouter} from 'react-router-relay';
+import App from './components/App';
+import ViewerQueries from './queries/ViewerQueries';
 
-ReactDOM.render(
-  <Relay.RootContainer
-    Component={App}
-    route={new AppHomeRoute()}
-  />,
+
+
+ReactDOM.render (
+  <RelayRouter history={createHashHistory({queryKey: false})}>
+    <Route
+      path="/" component={App}
+      queries={ViewerQueries}>
+      <IndexRoute
+        component={App}
+        queries={ViewerQueries}
+      />
+    </Route>
+  </RelayRouter>,
   document.getElementById('root')
 );
